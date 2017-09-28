9f863613f773602a45024e3e2f8768d2f7829dc7
Drew Mercer <japanexpress@gmail.com>
2017-09-27 20:43:36 -0400

    Starting server file

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+var PORT = process.env.PORT || 3300; // Sets an initial port.
+
+app.use(express.static('app/public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+
+var friends = require('./app/data/friends.js');
