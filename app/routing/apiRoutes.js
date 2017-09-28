2347d6bfc828efb2adeb796e3a2b79d2e4ca0838
Drew Mercer <japanexpress@gmail.com>
2017-09-27 22:33:46 -0400

    Adding some comments for clarity

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,12 @@
 var friends = require('../data/friends.js');
 
 module.exports = function(app) {
+    // GET route
   app.get('/api/friends', function(req, res) {
     res.json(friends);
   });
 
+  // POST route, handling incoming survey results and compatibility logic
   app.post('/api/friends', function(req, res) {
     var newFriend = req.body;
     var bestMatch = {};
