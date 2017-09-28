e70d8a8b19b8cebb032448c14d1128b4a98f1aea
Drew Mercer <japanexpress@gmail.com>
2017-09-27 22:35:59 -0400

    Adding another default but opposite friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -4,6 +4,12 @@ var friends = [
     photo:
       'https://yt3.ggpht.com/-uJh4oSQAwak/AAAAAAAAAAI/AAAAAAAAAAA/AMGKfKvDP3w/s900-c-k-no-mo-rj-c0xffffff/photo.jpg',
     scores: [5, 5, 1, 1, 5, 4, 1, 3, 5, 5]
+  },
+  {
+    name: 'Ahmed T',
+    photo:
+      'https://pbs.twimg.com/profile_images/779129231028682752/ijbnCAIa.jpg',
+    scores: [1, 1, 5, 5, 1, 2, 5, 3, 1, 1]
   }
 ];
 
