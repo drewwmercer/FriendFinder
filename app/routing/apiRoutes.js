22f1fa4e42c5d7cce81b68f90fe63f2f2e0ab66e
Drew Mercer <japanexpress@gmail.com>
2017-09-27 20:57:55 -0400

    Setting up api routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,7 +1,30 @@
 var friends = require('../data/friends.js');
 
-module.exports = function (app) {
-  app.get('/api/friends', function (req,res) {
-      res.json(friends);
+module.exports = function(app) {
+  app.get('/api/friends', function(req, res) {
+    res.json(friends);
   });
-  
+
+  app.post('/api/friends', function(req, res) {
+    var newFriend = req.body;
+
+    var bestMatch = {};
+
+    for (var i = 0; i < newFriend.scores.length; i++) {
+      if (newFriend.scores[i] == '1 (Strongly Disagree)') {
+        newFriend.scores[i] = 1;
+      } else if (newFriend.scores[i] == '5 (Strongly Agree)') {
+        newFriend.scores[i] = 5;
+      } else {
+        newFriend.scores[i] = parseInt(newFriend.scores[i]);
+      }
+    }
+
+
+    bestMatch = friends[bestMatchIndex];
+
+    friends.push(newFriend);
+
+    res.json(bestMatch);
+  });
+};
