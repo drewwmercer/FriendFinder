74b26b34c16889ba56afba42b5036294a34db8aa
Drew Mercer <japanexpress@gmail.com>
2017-09-27 21:30:21 -0400

    Adding a deafult friend to start with

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,10 @@
-var friends = [];
+var friends = [
+  {
+    name: 'Bob B',
+    photo:
+      'https://yt3.ggpht.com/-uJh4oSQAwak/AAAAAAAAAAI/AAAAAAAAAAA/AMGKfKvDP3w/s900-c-k-no-mo-rj-c0xffffff/photo.jpg',
+    scores: [5, 5, 1, 1, 5, 4, 1, 3, 5, 5]
+  }
+];
 
 module.exports = friends;
